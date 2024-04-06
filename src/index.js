0dd1ae3e625ff1a4eba5aab621ff7adbc3772983
Nick Sanchez <nicksanchez2371@gmail.com>
2024-04-06 01:38:22 -0500

    added comments on how to view the project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+//KEEP APP LOGIC SEPARATE FROM DOM RELATED
+
+//to do list will have a title, description, due date, priority. maybe notes and checklist
+
+//they will be able to create a section similar to a folder. where users can decide where to put there list.
+//there will already by one by default for them.
+
+//USER INTERFACE SHOULD BE ABLE TO DO THESE
+//view all projects;
+//view all todos inside projects;
+//expand a single todo to see more info
+//delete a todo
+
